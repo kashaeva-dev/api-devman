fb4156d028f99a5643ac7914f948200761d25367
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2023-09-22 16:19:49 +0300

    Add timestamp

--- main.py
+++ main.py
@@ -4,23 +4,34 @@ import requests
 from environs import Env
 
 
-def check_api_devman(token):
+def check_api_devman(token, params):
     url = 'https://dvmn.org/api/long_polling/'
     headers = {
         'Authorization': f'Token {token}',
     }
-    response = requests.get(url, headers=headers, timeout=5)
-    response.raise_for_status()
-    print(response.text)
+    if params:
+        response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+    else:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+    return response.json()
 
 
 def main():
     env = Env()
     env.read_env()
     devman_token = env.str('DEVMAN_TOKEN')
+    params = {}
     while True:
-        with suppress(requests.exceptions.ReadTimeout):
-            check_api_devman(devman_token)
+        with suppress(requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout):
+            response = check_api_devman(devman_token, params)
+            print(response)
+        if response['status'] == 'timeout':
+            timestamp_to_request = response['timestamp_to_request']
+            params = {
+                'timestamp_to_request': timestamp_to_request,
+            }
 
 
 if __name__ == "__main__":
