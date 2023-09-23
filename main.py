2b9d233db0ff325610d63b16e76a54a38caace63
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2023-09-23 23:54:09 +0300

    Minor changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from contextlib import suppress
+import time
+from textwrap import dedent
 
 import requests
 import telegram
@@ -10,39 +11,44 @@ def check_api_devman(token, params):
     headers = {
         'Authorization': f'Token {token}',
     }
-    if params:
-        response = requests.get(url, headers=headers, params=params)
-        response.raise_for_status()
-    else:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
     return response.json()
 
 
 def check_reviews(devman_token, params, bot, tg_chat_id):
     while True:
-        with suppress(requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout):
-            response = check_api_devman(devman_token, params)
-        if response['status'] == 'timeout':
-            timestamp_to_request = response['timestamp_to_request']
-            params = {
-                'timestamp_to_request': timestamp_to_request,
-            }
+        try:
+            reviews = check_api_devman(devman_token, params)
+        except requests.exceptions.ReadTimeout:
+            continue
+        except requests.exceptions.ConnectionError:
+            time.sleep(30)
         else:
-            review = response['new_attempts'][0]
-            if review['is_negative']:
-                bot.send_message(chat_id=tg_chat_id,
-                                 text=f'Преподаватель проверил работу *"{review["lesson_title"]}"*\.\n\n'
-                                      'К сожалению, в работе нашлись ошибки\.\n'
-                                      f'[Ссылка на работу]({review["lesson_url"]})',
-                                 parse_mode=telegram.ParseMode.MARKDOWN_V2,
-                                 )
+            if reviews['status'] == 'timeout':
+                timestamp_to_request = reviews['timestamp_to_request']
+                params = {
+                    'timestamp': timestamp_to_request,
+                }
             else:
-                bot.send_message(chat_id=tg_chat_id,
-                                 text=f'Преподаватель проверил работу *"{review["lesson_title"]}"*\.\n\n'
-                                      'Преподавателю все понравилось, можно приступать к следующему уроку\!',
-                                 parse_mode=telegram.ParseMode.MARKDOWN_V2,
-                                 )
+                current_review = reviews['new_attempts'][0]
+                if current_review['is_negative']:
+                    bot.send_message(chat_id=tg_chat_id,
+                                     text=dedent(f"""
+                                     Преподаватель проверил работу *"{current_review["lesson_title"]}"*\.
+                                     К сожалению, в работе нашлись ошибки\.
+                                     [Ссылка на работу]({current_review["lesson_url"]})
+                                     """),
+                                     parse_mode=telegram.ParseMode.MARKDOWN_V2,
+                                     )
+                else:
+                    bot.send_message(chat_id=tg_chat_id,
+                                     text=dedent(f"""
+                                     Преподаватель проверил работу *"{current_review["lesson_title"]}"*\.
+                                     Преподавателю все понравилось, можно приступать к следующему уроку\.
+                                     """),
+                                     parse_mode=telegram.ParseMode.MARKDOWN_V2,
+                                     )
 
 
 if __name__ == "__main__":
