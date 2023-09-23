53f0858d67d2888fa24bf725fdb483a20220fe9e
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2023-09-23 22:43:00 +0300

    Fix chat_id

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def check_api_devman(token, params):
     return response.json()
 
 
-def check_reviews(devman_token, params, bot, chat_id):
+def check_reviews(devman_token, params, bot, tg_chat_id):
     while True:
         with suppress(requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout):
             response = check_api_devman(devman_token, params)
@@ -31,14 +31,14 @@ def check_reviews(devman_token, params, bot, chat_id):
         else:
             review = response['new_attempts'][0]
             if review['is_negative']:
-                bot.send_message(chat_id=chat_id,
+                bot.send_message(chat_id=tg_chat_id,
                                  text=f'Преподаватель проверил работу *"{review["lesson_title"]}"*\.\n\n'
                                       'К сожалению, в работе нашлись ошибки\.\n'
                                       f'[Ссылка на работу]({review["lesson_url"]})',
                                  parse_mode=telegram.ParseMode.MARKDOWN_V2,
                                  )
             else:
-                bot.send_message(chat_id=chat_id,
+                bot.send_message(chat_id=tg_chat_id,
                                  text=f'Преподаватель проверил работу *"{review["lesson_title"]}"*\.\n\n'
                                       'Преподавателю все понравилось, можно приступать к следующему уроку\!',
                                  parse_mode=telegram.ParseMode.MARKDOWN_V2,
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     env.read_env()
     devman_token = env.str('DEVMAN_TOKEN')
     tg_bot_token = env('TG_BOT_TOKEN')
-    chat_id = env('CHAT_ID')
+    tg_chat_id = env('TG_CHAT_ID')
     params = {}
     bot = telegram.Bot(token=tg_bot_token)
-    check_reviews(devman_token, params, bot, chat_id)
+    check_reviews(devman_token, params, bot, tg_chat_id)
